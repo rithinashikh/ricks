6adcd51f7f8d5320275c856b0b3be0246289be5e
Rithin Ashikh <rithinashikhev@gmail.com>
2023-03-11 17:47:28 +0530

    six

--- ricks/settings.py
+++ ricks/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0lyje&ifjw6te(opo4anvcoao$o^jpc86ok60l$7^!&yjnsx1w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
